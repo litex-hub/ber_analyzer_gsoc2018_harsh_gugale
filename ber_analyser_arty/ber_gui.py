bae2443f355cfcc1a84f8c09f1aa07237de44b74
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-07-31 22:26:25 +0530

    Event handler class for GUI

--- ber_analyser_arty/ber_gui.py
+++ ber_analyser_arty/ber_gui.py
@@ -14,29 +14,114 @@ class Ui(Ui_MainWindow):
         self.setupUi(MainWindow)
         self.attachHandlers()
         self.prcon.BERinit()
-        self.encodingEnabled = False
+        self.pllStatusCheck()
+        self.checklinkstatus()
         self.updateBER()                    
 
     def attachHandlers(self):
-        self.comboBox.activated.connect(self.handleActivatedTx)
-        self.comboBox_3.activated.connect(self.handleActivatedRx)
-        self.comboBox_2.activated.connect(self.handleActivatedErr)
-        self.pushButton.clicked.connect(self.button_pressed)
+        self.comboBox_4.activated.connect(self.handleActivatedTx)
+        self.comboBox_5.activated.connect(self.handleActivatedRx)
+        self.comboBox_6.activated.connect(self.handleActivatedErr)
+        self.txpolcheckbox.stateChanged.connect(self.txpolchange)
+        self.rxpolcheckbox.stateChanged.connect(self.rxpolchange)
+        self.loopbackcheck.stateChanged.connect(self.loopbackmode)
+        self.txrst.clicked.connect(self.TxReset)
+        self.rxrst.clicked.connect(self.RxReset)
+        self.submitbutton.clicked.connect(self.drpReadWrite)
+        self.swingcombo.activated.connect(self.changeSwing)
+        self.precombo.activated.connect(self.changePrecursor)
+        self.postcombo.activated.connect(self.changePostcursor)
+
+    def drpReadWrite(self):
+        drp_addr = int(self.drpaddr.text(),16)
+        drp_value = int(self.drpval.text(),16)
+
+        if drpreadradio.isChecked == True:
+            self.prcon.drpWrite(drp_addr,drp_value)
+
+        if drpwriteradio.isChecked == True:
+            drp_value = self.prcon.drpRead(drp_addr)
+            self.drpval.setText(hex(drp_value))
+
+    def loopbackmode(self,state):
+        if state == QtCore.Qt.Checked:
+            self.enableLoopback()
+        else:
+            self.disableLoopback()
+
+    def txpolchange(self,state):
+        if state == QtCore.Qt.Checked:
+            self.txPolarity(True)
+        else:
+            self.txPolarity(False)
+
+    def rxpolchange(self,state):
+        if state == QtCore.Qt.Checked:
+            self.rxPolarity(True)
+        else:
+            self.rxPolarity(False)
+
+    def changeSwing(self,index):
+        if index == 0:
+            self.prcon.changeOutputSwing(0b0001)
+        if index == 1:
+            self.prcon.changeOutputSwing(0b0100)
+        if index == 2:
+            self.prcon.changeOutputSwing(0b1000)
+        if index == 3:
+            self.prcon.changeOutputSwing(0b1011)
+        if index == 4:
+            self.prcon.changeOutputSwing(0b1111)
+
+    def changePrecursor(self,index):
+        if index == 0:
+            self.prcon.changetxPrecursor(0b00000)
+        if index == 1:
+            self.prcon.changetxPrecursor(0b00010)
+        if index == 2:
+            self.prcon.changetxPrecursor(0b00101)
+        if index == 3:
+            self.prcon.changetxPrecursor(0b01000)
+        if index == 4:
+            self.prcon.changetxPrecursor(0b01111)
+        if index == 5:
+            self.prcon.changetxPrecursor(0b10100)
+
+    def changePostcursor(self,index):
+        if index == 0:
+            self.prcon.changetxPostcursor(0b00000)
+        if index == 1:
+            self.prcon.changetxPostcursor(0b00010)
+        if index == 2:
+            self.prcon.changetxPostcursor(0b00100)
+        if index == 3:
+            self.prcon.changetxPostcursor(0b01000)
+        if index == 4:
+            self.prcon.changetxPostcursor(0b01111)
+        if index == 5:
+            self.prcon.changetxPostcursor(0b10100)
+        if index == 6:
+            self.prcon.changetxPostcursor(0b11000)
+        if index == 7:
+            self.prcon.changetxPostcursor(0b11110)
+
+    def pllStatusCheck(self):
+        self.plllabel.setText(prcon.PLLlockStatus())
+        QtCore.QTimer.singleShot(5000,self.updateBER)
 
     def updateBER(self):
-        self.label_4.setText(str(round(self.prcon.calcBER(40),3)))
+        self.label_4.setText(str(round(self.prcon.calcBER(),3)))
         QtCore.QTimer.singleShot(1000,self.updateBER)
 
-    def button_pressed(self):
-        if self.encodingEnabled is True:
-            self.prcon.disable8b10b()
-            self.pushButton.setText("Enable")
-            self.encodingEnabled = False
+    def checklinkstatus(self):
+        self.linklabel.setText(prcon.checkMGTLink())
+        QtCore.QTimer.singleShot(5000,self.checklinkstatus)
 
-        else:
-            self.prcon.enable8b10b()
-            self.pushButton.setText("Disable")
-            self.encodingEnabled = True
+    def TxReset(self):
+        self.prcon.resetTx()
+
+    def RxReset(self):
+        self.prcon.resetRx()
 
     def handleActivatedTx(self,index):
         if index == 0:
