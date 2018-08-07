666b0d24e1d426e3932d94e066b9592360f1a23e
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-08-07 23:00:37 +0530

    Added Variable linerate support

--- te014.py
+++ te014.py
@@ -12,13 +12,14 @@ from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.soc.cores import dna
 
+from math import *
 from top_gtp import *
 
 from litex.build.generic_platform import *
 from litex.build.xilinx import XilinxPlatform
 
 _io = [
-    ("clk25", 0, Pins("T14"), IOStandard("LVCMOS33")),
+    ("clk_in", 0, Pins("T14"), IOStandard("LVCMOS33")),
 
     # serial
     ("serial", 0,
@@ -53,57 +54,34 @@ class Platform(XilinxPlatform):
                 """)
 
 
-def period_ns(freq):
-    return 1e9/freq
-
-
 class _CRG(Module):
-    def __init__(self, platform):
+    def __init__(self, platform,pll):
         self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_clk200 = ClockDomain()
 
-        clk25 = platform.request("clk25")
-        clk25.attr.add("keep")
-        platform.add_period_constraint(clk25, period_ns(25e6))
+        clk_in = platform.request("clk_in")
+        clk_in.attr.add("keep")
+        platform.add_period_constraint(clk_in,pll["fin_period"])
 
         pll_locked = Signal()
         pll_fb = Signal()
         pll_sys = Signal()
-        pll_clk200 = Signal()
         self.specials += [
             Instance("PLLE2_BASE",
                      p_STARTUP_WAIT="FALSE", o_LOCKED=pll_locked,
 
-                     # VCO @ 1600 MHz
-                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=40.0,
-                     p_CLKFBOUT_MULT=64, p_DIVCLK_DIVIDE=1,
-                     i_CLKIN1=clk25, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
+                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=pll["fin_period"],
+                     p_CLKFBOUT_MULT=pll["m"], p_DIVCLK_DIVIDE=pll["d"],
+                     i_CLKIN1=clk_in, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
 
                      # 100 MHz
-                     p_CLKOUT0_DIVIDE=16, p_CLKOUT0_PHASE=0.0,
+                     p_CLKOUT0_DIVIDE=pll["o"], p_CLKOUT0_PHASE=0.0,
                      o_CLKOUT0=pll_sys,
-
-                     # 200 MHz
-                     p_CLKOUT1_DIVIDE=8, p_CLKOUT1_PHASE=0.0,
-                     o_CLKOUT1=pll_clk200
             ),
             Instance("BUFG", i_I=pll_sys, o_O=self.cd_sys.clk),
-            Instance("BUFG", i_I=pll_clk200, o_O=self.cd_clk200.clk),
             AsyncResetSynchronizer(self.cd_sys, ~pll_locked),
-            AsyncResetSynchronizer(self.cd_clk200, ~pll_locked),
         ]
 
         platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks REQP-49]")
-
-        reset_counter = Signal(4, reset=15)
-        ic_reset = Signal(reset=1)
-        self.sync.clk200 += \
-            If(reset_counter != 0,
-                reset_counter.eq(reset_counter - 1)
-            ).Else(
-                ic_reset.eq(0)
-            )
-        self.specials += Instance("IDELAYCTRL", i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
 
 def csr_map_update(csr_map, csr_peripherals):
     csr_map.update(dict((n, v)
@@ -117,9 +95,9 @@ class TE014SoC(SoCCore):
     }
     csr_map_update(SoCCore.csr_map, csr_peripherals)
 
-    def __init__(self):
+    def __init__(self,pll):
         platform = Platform()
-        sys_clk_freq = int(100e6)
+        sys_clk_freq = int(pll["refclk_freq"])
 
         SoCCore.__init__(self, platform,
             cpu_type=None,
@@ -127,10 +105,10 @@ class TE014SoC(SoCCore):
             csr_data_width=32,
             with_uart=False,
             with_timer=False,
-            ident="TE014SoC Example Design", ident_version=True)
+            ident="Bit Error Ratio Analyzer", ident_version=True)
 
         # crg
-        self.submodules.crg = _CRG(platform)
+        self.submodules.crg = _CRG(platform,pll)
 
         # No CPU, use Serial to control Wishbone bus
         self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"), sys_clk_freq, baudrate=115200))
@@ -139,15 +117,46 @@ class TE014SoC(SoCCore):
         # dna
         self.submodules.dna = dna.DNA()
 
-        self.submodules.top_gtp = Top_gtp(self.crg.cd_sys.clk,100e6, 2e9, platform)
+        self.submodules.top_gtp = Top_gtp(self.crg.cd_sys.clk,pll["refclk_freq"], pll["linerate"], platform)
+
+def pllSettings(linerate,fin):
+	allowed_linerates = [i*5e8 for i in range(2,13)]
+	if linerate not in allowed_linerates:
+		raise ValueError("Allowed linerates : 1GHz - 6GHz in increments of 0.5GHz")
+
+	required_refclk = linerate/20
+
+	fvcomax = 1600e6
+	fvcomin = 800e6
+	fpfdmin = 19e6
+	fpfdmax = 500e6
+
+	dmin = ceil(fin/fpfdmax)
+
+	assert dmin<=106 and dmin>=1
+
+	mideal = round(((dmin*fvcomax)/fin),3)
+	assert mideal<=64 and mideal>=2
+
+	o = round(((fin*mideal)/(dmin*required_refclk)),3)
+	assert o<=128 and o>=1
+
+	fin_period = round((1e9/fin),3)
+
+	print("D : {0:8.5f} \nM : {1:8.5f} \nO : {2:8.5f}".format(dmin,mideal,o))
+
+	return {"d":dmin,"m":mideal,"o":o,"fin":fin,"fin_period":fin_period,"refclk_freq":required_refclk,"linerate":linerate}
 
 
 def main():
-    args = sys.argv[1:]
-    no_compile = "no_compile" in args
-    soc = TE014SoC()
-    builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
-    vns = builder.build()
+	linerate = 2e9
+	fin = 25e6
+	args = sys.argv[1:]
+	no_compile = "no_compile" in args
+	pll = pllSettings(linerate,fin)
+	soc = TE014SoC(pll)
+	builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
+	vns = builder.build()
 
 if __name__ == "__main__":
-    main()
+	main()
